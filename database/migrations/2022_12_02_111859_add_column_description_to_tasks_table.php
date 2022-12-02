879fe516588678a09eee6e9101b397e07dbedff8
valdidev <valdicode.fernando@gmail.com>
2022-12-02 12:32:13 +0100

    migrate and migrate rollback

--- database/migrations/2022_12_02_111859_add_column_description_to_tasks_table.php
+++ database/migrations/2022_12_02_111859_add_column_description_to_tasks_table.php
@@ -13,14 +13,8 @@ return new class extends Migration
      */
     public function up()
     {
-        Schema::create('users', function (Blueprint $table) {
-            $table->id();
-            $table->string('name');
-            $table->string('email')->unique();
-            $table->timestamp('email_verified_at')->nullable();
-            $table->string('password');
-            $table->rememberToken();
-            $table->timestamps();
+        Schema::table('tasks', function (Blueprint $table) {
+            $table -> text('description') -> after('id');
         });
     }
 
@@ -31,6 +25,8 @@ return new class extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('users');
+        Schema::table('tasks', function (Blueprint $table) {
+            $table -> dropColumn('description');
+        });
     }
 };
