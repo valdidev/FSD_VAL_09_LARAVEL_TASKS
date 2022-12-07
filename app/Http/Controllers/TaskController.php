ef72960e9cc5c8d60ab56f68f3426b246504a185
valdidev <valdicode.fernando@gmail.com>
2022-12-07 11:35:08 +0100

    ft: get all tasks controller

--- app/Http/Controllers/TaskController.php
+++ app/Http/Controllers/TaskController.php
@@ -2,11 +2,39 @@
 
 namespace App\Http\Controllers;
 
-use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Log;
+use PhpParser\Node\Stmt\TryCatch;
+
+// use Illuminate\Http\Request;
 
 class TaskController extends Controller
 {
-    public function createTask () {
+    public function createTask()
+    {
         return 'create task';
+    }
+
+    public function getTasks()
+    {
+        Log::info('Getting all tasks');
+        try {
+            $tasks = DB::select('select * from tasks');
+
+            return response([
+                'success' => true,
+                'message' => 'All tasks retrieved ok',
+                'data' => $tasks
+            ], 200);
+        } catch (\Throwable $th) {
+
+            Log::error($th->getMessage());
+
+            return response([
+                'success' => true,
+                'message' => 'All tasks retrieved ok',
+                'data' => 'No se han podido recuperar las tareas'
+            ], 500);
+        }
     }
 }
--- routes/api.php
+++ routes/api.php
@@ -1,27 +1,16 @@
 <?php
 
 use App\Http\Controllers\TaskController;
-use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
-
-/*
-|--------------------------------------------------------------------------
-| API Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register API routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| is assigned the "api" middleware group. Enjoy building your API!
-|
-*/
+// use Illuminate\Http\Request;
 
 /* Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 }); */
 
-Route::get('/tasks', function () {
+/* Route::get('/tasks', function () {
     return ['tasks to my tasks app'];
-});
+}); */
 
 Route::post('/tasks', function () {
     return 'posts tasks';
@@ -33,10 +22,11 @@ Route::put('/tasks', function () {
     return 'updating tasks';
 });
 
-
 Route::delete('/tasks', function () {
     return 'deleting tasks';
 });
+
+Route::get('/tasks', [TaskController::class, 'getTasks']);
 
 Route::get('/task/{id}', function ($id) {
     return ['see task id: '.$id];
